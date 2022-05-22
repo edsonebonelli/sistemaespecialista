043c6b19e96fa14b5e186c1345f2e5af02806249
Edson Bonelli <98748057+edsonebonelli@users.noreply.github.com>
2022-05-22 15:15:22 -0300

    Update sistemaespecialista.R

--- sistemaespecialista.R
+++ sistemaespecialista.R
@@ -1,13 +1,9 @@
-#instala pacote
 install.packages("sets", dependencies=T)
 
-#carrega o pacote
 library(sets)
 
-#Define Universo
 sets_options("universe", seq(1, 100, 1))
 
-#criacao de variaveis
 variaveis <- set(
   Frequencia = fuzzy_partition(varnames = c( MenDSemanas  = 30, MaiDSemanas = 60, Diario = 70, Continuo=90), radius=20, FUN = fuzzy_cone),
   SABA = fuzzy_partition(varnames = c(MenDSemanas= 20, MaiDSemanas = 30, Diario = 70, DuasxDia=90), sd = 10),
@@ -15,7 +11,6 @@ variaveis <- set(
   Classificacao = fuzzy_partition(varnames = c(Moderada = 20, AgudaGrave=40  , RiscoVida = 60), sd=10)
 )
 
-#definicao das regras
 regras <-
   set(
     fuzzy_rule( Frequencia %is% MenDSemanas && SABA %is% MenDSemanas && DebitoExp %is% CinqOiten, Classificacao %is% Moderada ),
@@ -36,25 +31,17 @@ regras <-
     fuzzy_rule( Frequencia %is% Continuo && SABA %is% DuasxDia && DebitoExp %is% MaisTrintT, Classificacao %is% RiscoVida )
   )
 
-#construindo o sistema
 sistema <- fuzzy_system(variaveis, regras)
 sistema
 plot(sistema)
 
-#fazendo inferencia
 inferencia <- fuzzy_inference(sistema, list(Frequencia  = 80 , SABA = 70, DebitoExp = 80 ))
 inferencia
 plot(inferencia)
 
-#defuzication
-# method = c("meanofmax", "smallestofmax", "largestofmax", "centroid"))
 def = gset_defuzzify(inferencia, "centroid")
 
-
-#analisando o resultado
 plot(sistema$variables$Classificacao)
 lines(inferencia, col = "red", lwd=4)
 
-
-#desfazendo o universo
 sets_options("universe", NULL)
